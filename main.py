16e44a2d023b3af2680b3be1a2d6f4d0a044b6ce
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-07-31 16:43:58 +0300

    added clear button

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ def Calculate():
     except:
         messagebox.showerror('Invalid input', 'Your need to enter an equation!')
 
+def clear():
+    equ_entry.delete(0, ttk.END)
+    equ_entry.focus()
+
 
 window = ttk.Window(themename='minty')
 window.title("find your x")
@@ -30,6 +34,7 @@ input_frame = ttk.Frame(master=window,padding=50)
 button_frame = ttk.Frame(master=window)
 label = ttk.Label(master=input_frame,text='Enter your Equation')
 equ_entry = ttk.Entry(master=input_frame,justify='center',textvariable=equ)
+clear_button = ttk.Button(master=window,text="\u232B",command=clear,padding=10)
 cal_button = ttk.Button(master=window,text="Calculate",command=Calculate,padding=10)
 x_label = ttk.Label(master=window,textvariable=output,padding=10)
 
@@ -38,6 +43,7 @@ input_frame.pack()
 label.pack()
 equ_entry.pack(side='left')
 equ_entry.focus()
+clear_button.pack()
 button_frame.pack()
 cal_button.pack(pady=10)
 x_label.pack()
